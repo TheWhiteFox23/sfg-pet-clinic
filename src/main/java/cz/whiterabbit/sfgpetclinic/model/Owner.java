d1d69b95b6307a9421a4252bc631dd3332cc8613
theWhiteFox23 <SinglePixelCircle@gmail.com>
2020-08-03 18:53:45 +0200

    Adding pojo for the model

---
+++ src/main/java/cz/whiterabbit/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package cz.whiterabbit.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/cz/whiterabbit/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package cz.whiterabbit.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
