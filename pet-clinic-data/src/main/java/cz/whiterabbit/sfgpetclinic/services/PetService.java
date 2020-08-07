af2efdbe229b6fe01a3c66705af536723c952a04
TheWhiteFox23 <SinglePixelCircle@gmail.com>
2020-08-07 06:54:48 +0200

    implemeneted interfaces for model. Closes #5

---
+++ pet-clinic-data/src/main/java/cz/whiterabbit/sfgpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package cz.whiterabbit.sfgpetclinic.services;
+
+import cz.whiterabbit.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/cz/whiterabbit/sfgpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package cz.whiterabbit.sfgpetclinic.services;
+
+import cz.whiterabbit.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
