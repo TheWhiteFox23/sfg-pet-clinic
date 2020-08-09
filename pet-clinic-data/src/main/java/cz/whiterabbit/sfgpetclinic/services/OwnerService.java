43fe4bc631b30b2a45739e804a6ab1e75e1ca379
theWhiteFox23 <SinglePixelCircle@gmail.com>
2020-08-09 10:31:24 +0200

    Implement common service nterface. Closes #11

--- pet-clinic-data/src/main/java/cz/whiterabbit/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/cz/whiterabbit/sfgpetclinic/services/OwnerService.java
@@ -3,9 +3,6 @@ package cz.whiterabbit.sfgpetclinic.services;
 import cz.whiterabbit.sfgpetclinic.model.Owner;
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
