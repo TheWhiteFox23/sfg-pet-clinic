8e1318f1fbe50668ee8efa73844ae60800cb5960
TheWhiteFox23 <SinglePixelCircle@gmail.com>
2020-08-13 07:15:40 +0200

    Create Vet Speciality Entity and Asociate to the Vet. Closes #27

--- pet-clinic-data/src/main/java/cz/whiterabbit/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/cz/whiterabbit/sfgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package cz.whiterabbit.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+    private Set<VetSpeciality> specialities;
+
+    public Set<VetSpeciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<VetSpeciality> specialities) {
+        this.specialities = specialities;
+    }
 }
---
+++ pet-clinic-data/src/main/java/cz/whiterabbit/sfgpetclinic/model/VetSpeciality.java
@@ -0,0 +1,13 @@
+package cz.whiterabbit.sfgpetclinic.model;
+
+public class VetSpeciality extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
