12ed041c4ac7221a0ee16b177f1bf7e8a9f44b0d
TheWhiteFox23 <SinglePixelCircle@gmail.com>
2020-08-13 07:35:28 +0200

    Add pet types in with Bootstrap (startup). Closes #32

--- pet-clinic-web/src/main/java/cz/whiterabbit/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/cz/whiterabbit/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package cz.whiterabbit.sfgpetclinic.bootstrap;
 
 import cz.whiterabbit.sfgpetclinic.model.Owner;
+import cz.whiterabbit.sfgpetclinic.model.PetType;
 import cz.whiterabbit.sfgpetclinic.model.Vet;
 import cz.whiterabbit.sfgpetclinic.services.OwnerService;
+import cz.whiterabbit.sfgpetclinic.services.PetTypeService;
 import cz.whiterabbit.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner = new Owner();
         owner.setFirstName("John");
         owner.setLastName("Doe");
