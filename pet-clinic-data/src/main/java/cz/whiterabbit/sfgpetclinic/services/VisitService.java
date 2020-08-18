c4d37d80670c3d5aba3650661ce58f7b7d72e9bf
TheWhiteFox23 <SinglePixelCircle@gmail.com>
2020-08-18 13:33:53 +0200

    Create Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/cz/whiterabbit/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package cz.whiterabbit.sfgpetclinic.services;
+
+import cz.whiterabbit.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/cz/whiterabbit/sfgpetclinic/services/datajpa/VisitServiceJPA.java
@@ -0,0 +1,47 @@
+package cz.whiterabbit.sfgpetclinic.services.datajpa;
+
+import cz.whiterabbit.sfgpetclinic.model.Visit;
+import cz.whiterabbit.sfgpetclinic.repositories.VisitRepository;
+import cz.whiterabbit.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+@Profile("springdatajpa")
+public class VisitServiceJPA implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitServiceJPA(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
